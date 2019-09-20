4f7e4831cf3a1d9d88c052b7d837c13178960510
Riley Moore <rilemoore@gmail.com>
2019-09-20 12:48:53 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Monatgue",
+        name: "Lord Montague",
         age: "53"
         #your key/value pairs here
         },
